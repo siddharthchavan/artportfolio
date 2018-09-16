8515131d38eb1be1029d18613db94edb060a6bd5
siddharthchavan <siddharthc@alsionschool.org>
2018-09-16 12:28:42 -0700

    added some utility functions

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,14 +1,14 @@
-function print(text, outputID) {
+export function print(text, outputID) {
   var output = document.getElementById(outputID);
   output.innerHTML(text);
 }
 
-function getClass(className, node) {
+export function getClass(className, node) {
   node = document.getElementsByClassName(className);
   return classElements;
 }
 
-function getID(IDname, varName) {
+export function getID(IDname, varName) {
   varName = document.getElementById(IDname);
   return varName
 }
